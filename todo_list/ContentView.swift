4454afbac3f1019306a487a19871da2fb7a68fde
hzy <2196260918@qq.com>
2021-02-18 11:10:43 +0800

    创造单个备忘录

--- todo_list/ContentView.swift
+++ todo_list/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView(.vertical, showsIndicators: true, content: {
+                    VStack {
+                        ForEach(0..<10){ item in
+                            singlecardview(title:item.description)
+                            
+                        }.padding()
+
+
+                    }        })
+       
+    }
+}
+
+struct singlecardview:View {
+    @State var ischecked:Bool = false
+    @State var title:String = ""
+    @State var duedate:Date = Date()
+
+    var body: some View{
+        HStack {
+            Rectangle()
+                .frame(width: 8)
+                .foregroundColor(.blue)
+            VStack(alignment: .leading, spacing:6) {
+                Text(self.title)
+                    .font(.headline)
+                    .fontWeight(.heavy)
+                Text(self.duedate.description)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            .padding(.leading)
+            Spacer()
+            Image(systemName: self.ischecked ? "checkmark.square.fill": "square")
+    .imageScale(.large)
+    .padding(.trailing)
+                .onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                    self.ischecked.toggle()
+                })
+        }
+        .frame(height:80)
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 10,x:0,y:10 )
     }
 }
 
