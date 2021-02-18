eecdb4eefd742034a5df5fa11ef718cb822ebbf8
hzy <2196260918@qq.com>
2021-02-18 10:01:25 +0800

    Initial Commit

---
+++ todo_list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo_list
+//
+//  Created by ZY H on 2021/2/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todo_list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo_list
+//
+//  Created by ZY H on 2021/2/18.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
